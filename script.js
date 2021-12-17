9fac79aa3fd6d632bf96fa48eb42f8f098c12eca
Franconobile <eifrann22@gmail.com>
2021-12-16 21:34:42 -0300

    primer commit

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-// function toggleText() {
-//     let span = document.getElementById('showText');
-//     if (text.style.display === "none") {
-//         text.style.display = "block";
-//     } else {
-//         text.style.display = "none";
-//     }
-// }
-
-// function toggleText() {
-//     let text = document.getElementById('showText');
-//     if (text.style.display === "none") {
-//         text.style.display = "block";
-//     } 
-//     else {
-//         text.style.display = "none";
-//     }
-// }
-
-
-var elemento = document.getElementsByClassName('show');
-var textoMos = document.getElementsByClassName('showText')
-
-console.log(elemento);
-
-for(const elemet of elemento){
-    elemet.addEventListener('click', function() {
-        console.log("elemet");
-    })
-}
+var bandera = 1;
+var show = document.querySelectorAll('.show')
 
 
 
 
 
-
-
-
-
-// document.querySelectorAll('button')
-//   .forEach(toggleText => {
-//     toggleText.addEventListener('click', function() {
-//       const div = this.parentElement.querySelector('div');
-//       div.textContent = this.dataset.ans;
-//     });
-//   });
